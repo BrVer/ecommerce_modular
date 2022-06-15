f32ff7140646fd506450bd6054b39e12cdc6f193
Dmitry Vlasov <d.vlasov.work@gmail.com>
2022-06-15 15:11:45 +0200

    refactored subscription attacher

--- app/domains/subscription_attacher.rb
+++ app/domains/subscription_attacher.rb
@@ -1,32 +1,12 @@
 # frozen_string_literal: true
 
 class SubscriptionAttacher
-  SUBSCRIPTIONS = {
-    'Payments::Subscription': %i[
-      orders.order_accepted
-      orders.order_shipment_failed
-      orders.order_shipped
-    ],
-    'Inventory::Subscription': %i[
-      orders.order_submitted
-      orders.order_cancelled
-      orders.order_shipment_cancelled
-      orders.order_shipment_failed
-      orders.order_shipped
-    ],
-    'Orders::Subscription': %i[
-      inventory.reservation_success
-      inventory.reservation_failure
-      payments.authorization_failed
-      payments.authorization_expired
-      payments.payment_authorized
-    ]
-  }.freeze
-
-  def attach_subscriptions
-    SUBSCRIPTIONS.each do |subscription_name, messages|
-      subscription = subscription_name.to_s.constantize
-      messages.each { subscription.attach_to(_1) }
+  def attach_activesupport_subscriptions(config)
+    config.each do |subscribed_domain, emitted_events|
+      subscription = "#{subscribed_domain.capitalize}::Subscription".constantize
+      emitted_events.each do |domain, events_list|
+        events_list.each { subscription.attach_to("#{domain}.#{_1}") }
+      end
     end
   end
 end
--- config/initializers/subscriptions.rb
+++ config/initializers/subscriptions.rb
@@ -1,5 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.config.after_initialize do
-  SubscriptionAttacher.new.attach_subscriptions
+  subscriptions_file = 'config/subscriptions.yml'
+
+  if File.exist?(subscriptions_file)
+    config = YAML.load_file(subscriptions_file)
+
+    #TODO: only if not in Kafka mode!
+    SubscriptionAttacher.new.attach_activesupport_subscriptions(config)
+  end
 end
